300ecd64999db848c549103634e0ad73baafb6b2
Frank <f.neumann.berlin@googlemail.com>
2023-02-02 09:33:35 +0100

    Anfang OrderActivity.kt

--- app/src/main/java/com/example/orderapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/orderapp/ui/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.orderapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.ShareActionProvider
+
+import androidx.core.view.MenuItemCompat
 import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -16,6 +22,7 @@ import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var shareActionProvider: ShareActionProvider
     private lateinit var binding: ActivityMainBinding
     private lateinit var tabArray: Array<String>
 
@@ -54,6 +61,28 @@ class MainActivity : AppCompatActivity() {
         if(currentFragment != 0)
             binding.pager.setCurrentItem(currentFragment-1)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        val item = menu!!.findItem(R.id.action_share)
+        shareActionProvider = MenuItemCompat.getActionProvider(item) as ShareActionProvider
+        setShareAction("Das musst du unbedingt kennen lernen")
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_create_order -> startActivity(Intent(this, OrderActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setShareAction(text: String) {
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.type = "text/plain"
+        shareIntent.putExtra(Intent.EXTRA_TEXT,text)
+        shareActionProvider.setShareIntent(shareIntent)
     }
 
 
---
+++ app/src/main/java/com/example/orderapp/ui/OrderActivity.kt
@@ -0,0 +1,12 @@
+package com.example.orderapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.orderapp.R
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+    }
+}
